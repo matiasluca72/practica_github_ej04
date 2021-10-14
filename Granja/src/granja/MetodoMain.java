a3cd4370d63581ebb6f8ba0ceffa01a22a5f0201
matiasluca72 <matias_soto72@hotmail.com>
2021-10-14 18:39:58 -0300

    Primer commit

---
+++ Granja/src/granja/MetodoMain.java
@@ -0,0 +1,13 @@
+package granja;
+
+/**
+ *
+ * @author Matias Luca Soto
+ */
+public class MetodoMain {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
